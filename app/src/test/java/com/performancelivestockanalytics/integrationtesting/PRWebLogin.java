e799c3155ad643bf03b190b84cc27711bd084b52
Dawon Jeong <dawonjeong@cattlekrush.com>
2023-07-19 16:14:30 -0400

    #7 Add comments to PB/PRWebLogin + fix navigate to overview page

--- app/src/test/java/com/performancelivestockanalytics/integrationtesting/PRWebLogin.java
+++ app/src/test/java/com/performancelivestockanalytics/integrationtesting/PRWebLogin.java
@@ -1,5 +1,7 @@
 package com.performancelivestockanalytics.integrationtesting;
 
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.JavascriptExecutor;
@@ -17,18 +19,22 @@ public class PRWebLogin implements LogInInterface{
     private WebDriverWait wait;
     private WebDriver driver;
 
-    /**
-     * Constructor
-     */
     PRWebLogin() {
         this.setUp();
     }
 
     @Override
     public void setUp() {
+        /* This setup creates a chromedriver instead of using Safari because chromedriver
+           can be both used in mac + window environment. Also setting the driver to small
+           window size inorder to test all cases that users may experience.
+           */
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().setSize(new Dimension(500, 600));
+
+        // Use this WebDriverWait to make sure the driver is not trying to access an element that
+        // is not yet accessible within the view i.e. loading
         wait = new WebDriverWait(driver, TIMEWAIT);
     }
 
@@ -48,35 +54,33 @@ public class PRWebLogin implements LogInInterface{
      */
     @Override
     public void logIn(String targetServer, String username, String password) {
-        // Navigate to the url (Beef or Ranch)
+        // Pass in the targetServer to make the driver load the URL
         driver.get(targetServer);
 
-        // Pass in the username and password
-        checkVisibilityOrScroll(wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.id("user")))))
+        // Wait for the targetServer to load up then pass in the username and password
+        checkVisibilityOrScroll(wait.until(visibilityOfElementLocated(By.id("user"))))
                 .sendKeys(username);
-        checkVisibilityOrScroll(wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.id("pass")))))
+        checkVisibilityOrScroll(wait.until(visibilityOfElementLocated(By.id("pass"))))
                 .sendKeys(password);
 
-        // Click login button
-        checkVisibilityOrScroll(wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.className("login")))))
+        // After username and password has been inserted, move on by clicking login button
+        checkVisibilityOrScroll(wait.until(visibilityOfElementLocated(By.className("login"))))
                 .click();
 
         // Return errors when the username for login page is still visible (login not successful)
         try {
-            driver.findElement(By.id("accountButton"));
+            wait.until(visibilityOfElementLocated(By.id("accountButton")));
         }
         catch (NoSuchElementException nse) {
             // Success
         }
     }
 
-    /**
-     * Check if the element is visible, if not scroll to that element and return it
-     */
     private WebElement checkVisibilityOrScroll(WebElement element) {
         if (element.isDisplayed()) {
             return element;
         } else {
+            // The element is not displayed in current view, so scroll to that element
             JavascriptExecutor js = (JavascriptExecutor) driver;
             js.executeScript("arguments[0].scrollIntoView(true);", element);
 
--- app/src/test/java/com/performancelivestockanalytics/integrationtesting/PRWebNavigate.java
+++ app/src/test/java/com/performancelivestockanalytics/integrationtesting/PRWebNavigate.java
@@ -1,5 +1,7 @@
 package com.performancelivestockanalytics.integrationtesting;
 
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -12,35 +14,31 @@ public class PRWebNavigate {
     private WebDriverWait wait;
     private WebDriver driver;
 
-    /**
-     * Constructor
-     */
     PRWebNavigate(WebDriver driver) {
         this.driver = driver;
         wait = new WebDriverWait(driver, TIMEWAIT);
     }
 
-    /**
-     * Inside PR, navigate to overview page (home / default page)
-     */
     public void navigateToOverview() {
-        // Refresh the page. To remove the pop up or it doesn't see the left (bottom if window size is small) scroll bar (overview, herds, pastures, etc)
+        // Refresh the page to remove any pop-up so that navigation bar is visible
         driver.navigate().refresh();
 
-        // Navigate to overview page
-        checkVisibilityOrScroll(driver.findElement(By.className("nav-overview"))).click();
+        /* If refresh was successful, the navigation bar should be visible on the left or right (depending on the window size)
+           Click it to navigate to overview page
+           */
+        checkVisibilityOrScroll(wait.until(visibilityOfElementLocated(By.className("nav-overview")))).click();
 
-        // Check if page successfully navigated to the overview (Animals)
-        assert(driver.findElement(By.className("pull-right")).isDisplayed());
+        /* Now confirm the navigation to overview. If successful, the assert will pass.
+           Determine the success by the existence of animals(pull-right class)
+           */
+        assert(wait.until(visibilityOfElementLocated(By.className("pull-right"))).isDisplayed());
     }
 
-    /**
-     * Check if the element is visible, if not scroll to that element and return it
-     */
     private WebElement checkVisibilityOrScroll(WebElement element) {
         if (element.isDisplayed()) {
             return element;
         } else {
+            // The element is not displayed in current view, so scroll to that element
             JavascriptExecutor js = (JavascriptExecutor) driver;
             js.executeScript("arguments[0].scrollIntoView(true);", element);
 
