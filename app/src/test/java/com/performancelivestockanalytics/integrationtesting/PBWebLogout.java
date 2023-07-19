dc9819a6c434f5afc1673befabbf756983c5dc0a
Dawon Jeong <dawonjeong@cattlekrush.com>
2023-07-19 18:22:57 -0400

    #9 add comment + some fixes

--- app/src/test/java/com/performancelivestockanalytics/integrationtesting/PBWebLogout.java
+++ app/src/test/java/com/performancelivestockanalytics/integrationtesting/PBWebLogout.java
@@ -1,5 +1,7 @@
 package com.performancelivestockanalytics.integrationtesting;
 
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -11,35 +13,30 @@ public class PBWebLogout {
     private static final int TIMEWAIT = 3; // Normal timeout waiting for components
     private WebDriver driver;
     WebDriverWait wait;
-    JavascriptExecutor js;
 
-    /**
-     * Constructor
-     */
     PBWebLogout (WebDriver driver) {
         this.driver = driver;
-        js = (JavascriptExecutor) this.driver;
         wait = new WebDriverWait(driver, TIMEWAIT);
     }
 
     public void logOut() {
-        // Click Account Setting
-        checkVisibilityOrScroll(driver.findElement(By.className("account-settings-glyphicon"))).click();
+        // Assume that there is no pop-up and is able to access account setting when it's called
+        checkVisibilityOrScroll(wait.until(visibilityOfElementLocated(By.className("account-settings-glyphicon")))).click();
 
-        // Click Log Out Button
-        checkVisibilityOrScroll(driver.findElement(By.id("account_change"))).click();
+        // Wait for the account setting page to load, then click the log out button
+        checkVisibilityOrScroll(wait.until(visibilityOfElementLocated(By.id("account_change")))).click();
 
-        // Check if logged out successfully
-        assert(checkVisibilityOrScroll(driver.findElement(By.id("user"))).isDisplayed());
+        /* Now confirm the logout. If successful, the assert will pass.
+           Determine the success by the existence of username box in login page
+           */
+        assert(checkVisibilityOrScroll(wait.until(visibilityOfElementLocated(By.id("user")))).isDisplayed());
     }
 
-    /**
-     * Check if the element is visible, if not scroll to that element and return it
-     */
     private WebElement checkVisibilityOrScroll(WebElement element) {
         if (element.isDisplayed()) {
             return element;
         } else {
+            // The element is not displayed in current view, so scroll to that element
             JavascriptExecutor js = (JavascriptExecutor) driver;
             js.executeScript("arguments[0].scrollIntoView(true);", element);
 
