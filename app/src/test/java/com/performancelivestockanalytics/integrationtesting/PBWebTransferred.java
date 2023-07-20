1456bc00c2de553d9a261f1f6e1e1e582fc11742
Dawon Jeong <dawonjeong@cattlekrush.com>
2023-07-20 17:53:05 -0400

    Add comment + fixes

--- app/src/test/java/com/performancelivestockanalytics/integrationtesting/PBWebTransferred.java
+++ app/src/test/java/com/performancelivestockanalytics/integrationtesting/PBWebTransferred.java
@@ -15,23 +15,20 @@ public class PBWebTransferred {
     private static final int TIMEWAIT = 3; // Normal timeout waiting for components
     private WebDriver driver;
     WebDriverWait wait;
-    JavascriptExecutor js;
 
-    /**
-     * Constructor
-     */
     PBWebTransferred (WebDriver driver) {
         this.driver = driver;
-        js = (JavascriptExecutor) this.driver;
         wait = new WebDriverWait(driver, TIMEWAIT);
     }
 
     /**
-     * Check for the animal name inside the Group that the animal was added to
-     * In this case, Group "D-Group" is the destination (Currently Hardcoded)
-     * Navigates from Health tab -> targetGroup -> animalName
+     * Check for the animal name inside the Group that the animal was added to.
+     * To view the transferred animal, it is located in group inside the Health tab
      */
     public void checkTransferred(String animalName, String targetGroup) {
+        // We need to remove any pop-ups, so by refreshing the page it will remove them
+        driver.navigate().refresh();
+
         // Click the Health icon
         checkVisibilityOrScroll(wait.until(visibilityOfElementLocated(By.className("nav-health"))))
                 .click();
@@ -39,7 +36,7 @@ public class PBWebTransferred {
         // Wait for the Health page to load
         wait.until(visibilityOfElementLocated(By.className("component-name")));
 
-        // Click D-Group
+        // Since the button for targetGroup does not have a specific id, search through the list of elements that contains all the group name
         List<WebElement> group = driver.findElements(By.className("component-name"));
         for (WebElement el : group) {
             if (el.getText().equals(targetGroup)) {
@@ -48,22 +45,31 @@ public class PBWebTransferred {
             }
         }
 
-        // Search for the animal name
+        // Enter the animalName in the search bar
         checkVisibilityOrScroll(wait.until(visibilityOfElementLocated(By.cssSelector("[aria-controls='DataTables_Table_0']"))))
                 .sendKeys(animalName);
 
-        // Check if the animal name shows up
-        assert(checkVisibilityOrScroll(wait.until(visibilityOfElementLocated(By.className("sorting_1")))).getText()
-                .equals(animalName));
+        /* Now confirm the existence of the animal. If successful, the assert will pass.
+           Determine the success by the comparing the animal name that was searched to the animalName (param)
+           */
+        List<WebElement> tagID = driver.findElements(By.className("sorting_1"));
+        Boolean didAppear = false;
+        for (WebElement el : tagID) {
+            // There are additional spaces for Tag Id within the animal names
+            if (el.getText().trim().equals(animalName)) {
+                didAppear = true;
+                break;
+            }
+        }
+
+        assert(didAppear);
     }
 
-    /**
-     * Check if the element is visible, if not scroll to that element and return it
-     */
     private WebElement checkVisibilityOrScroll(WebElement element) {
         if (element.isDisplayed()) {
             return element;
         } else {
+            // The element is not displayed in current view, so scroll to that element
             JavascriptExecutor js = (JavascriptExecutor) driver;
             js.executeScript("arguments[0].scrollIntoView(true);", element);
 
