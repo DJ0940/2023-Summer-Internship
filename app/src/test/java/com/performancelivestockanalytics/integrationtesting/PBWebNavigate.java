e73b3c154de8775f87f407834b7fc92a8a49e8a8
Dawon Jeong <dawonjeong@cattlekrush.com>
2023-07-20 15:38:15 -0400

    #7 add comments

--- app/src/test/java/com/performancelivestockanalytics/integrationtesting/PBWebNavigate.java
+++ app/src/test/java/com/performancelivestockanalytics/integrationtesting/PBWebNavigate.java
@@ -1,5 +1,7 @@
 package com.performancelivestockanalytics.integrationtesting;
 
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -7,43 +9,38 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-/**
- * Performance Beef: Navigate to the overview page
- */
 public class PBWebNavigate {
     private static final int TIMEWAIT = 3;
     private WebDriverWait wait;
     private WebDriver driver;
 
-    /**
-     * Constructor
-     */
     PBWebNavigate(WebDriver driver) {
         this.driver = driver;
         wait = new WebDriverWait(driver, TIMEWAIT);
     }
 
-    /**
-     * Inside PB, navigate to overview page (home / default page)
-     */
     public void navigateToOverview() {
-        // Refresh the page. To remove the pop up or it doesn't see the left (bottom if window size is small) scroll bar (overview, sheets, delivery, etc)
+        // Refresh the page to remove any pop-up so that navigation bar is visible
         driver.navigate().refresh();
 
-        // Navigate to overview page
-        checkVisibilityOrScroll(driver.findElement(By.className("nav-overview"))).click();
+        /* If refresh was successful, the navigation bar should be visible on the left or right (depending on the window size)
+           Click it to navigate to overview page
+           */
+        checkVisibilityOrScroll(wait.until(visibilityOfElementLocated(By.className("nav-overview")))).click();
 
-        // Check if page navigated to the overview
-        assert(driver.findElement(By.id("btn_head_change")).isDisplayed());
+
+        /* Now confirm the navigation to overview. If successful, the assert will pass.
+           Determine the success by the existence of animals(pull-right class)
+           */
+        assert(wait.until(visibilityOfElementLocated(By.className("pull-right"))).isDisplayed());
+
     }
 
-    /**
-     * Check if the element is visible, if not scroll to that element and return it
-     */
     private WebElement checkVisibilityOrScroll(WebElement element) {
         if (element.isDisplayed()) {
             return element;
         } else {
+            // The element is not displayed in current view, so scroll to that element
             JavascriptExecutor js = (JavascriptExecutor) driver;
             js.executeScript("arguments[0].scrollIntoView(true);", element);
 
