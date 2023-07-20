9294b1e6fe60e3fa32134aa4bbcdc69348775a1b
Dawon Jeong <dawonjeong@cattlekrush.com>
2023-07-20 16:30:57 -0400

    PR Web Logout

--- app/src/test/java/com/performancelivestockanalytics/integrationtesting/PRWebLogout.java
+++ app/src/test/java/com/performancelivestockanalytics/integrationtesting/PRWebLogout.java
@@ -1,11 +1,11 @@
 package com.performancelivestockanalytics.integrationtesting;
 
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -15,40 +15,41 @@ public class PRWebLogout {
     private static final int TIMEWAIT = 3; // Normal timeout waiting for components
     private WebDriver driver;
     WebDriverWait wait;
-    JavascriptExecutor js;
 
-    /**
-     * Constructor
-     */
     PRWebLogout (WebDriver driver) {
         this.driver = driver;
-        js = (JavascriptExecutor) this.driver;
         wait = new WebDriverWait(driver, TIMEWAIT);
     }
 
     public void logOut() {
-        // Click Account Setting
-        checkVisibilityOrScroll(driver.findElement(By.className("account-settings-glyphicon"))).click();
+        // We need to remove any pop-ups, so by refreshing the page it will remove them and the account setting button will be visible
+        driver.navigate().refresh();
 
-        // Click Log Out Button
+        // After removing any pop-ups, navigate the user to the account setting where the log out button is located
+        checkVisibilityOrScroll(wait.until(visibilityOfElementLocated(By.className("account-settings-glyphicon")))).click();
+
+        // Since log out button does not have a specific id, search through the list of elements that has className as btn-primary
         List<WebElement> tempBtn = driver.findElements(By.className("btn-primary"));
         for (WebElement logout : tempBtn) {
             if (logout.getText().equals("Log Out")) {
                 checkVisibilityOrScroll(logout).click();
+
+                // Exit the loop to avoid stale element not found error
+                break;
             }
         }
 
-        // Check if logged out successfully
+        /* Now confirm the logout. If successful, the assert will pass.
+           Determine the success by the existence of username box in login page
+           */
         assert(checkVisibilityOrScroll(driver.findElement(By.id("user"))).isDisplayed());
     }
 
-    /**
-     * Check if the element is visible, if not scroll to that element and return it
-     */
     private WebElement checkVisibilityOrScroll(WebElement element) {
         if (element.isDisplayed()) {
             return element;
         } else {
+            // The element is not displayed in current view, so scroll to that element
             JavascriptExecutor js = (JavascriptExecutor) driver;
             js.executeScript("arguments[0].scrollIntoView(true);", element);
 
